3f6f3ce14ae1abaa06488a0e3fd07a289867afe9
seg <seg@soniclabs.com>
2024-11-16 00:31:04 +0400

    x

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,13 +1,1 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
-
-module.exports = {
-  solidity: "0.8.19",
-  networks: {
-    sonictest: {
-      url: "https://rpc.testnet.soniclabs.com",
-      accounts: [process.env.SONIC_PRIVATE_KEY],
-      chainId: 64165
-    }
-  }
-};
+// TODO
